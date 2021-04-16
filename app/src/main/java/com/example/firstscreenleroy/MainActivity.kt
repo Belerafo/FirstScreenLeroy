2cf8256f6ca41a184065bb056ee65cf29fc9f1ee
Lyshk <lyshka9@yandex.ru>
2021-04-16 20:36:30 +0300

    2

--- app/src/main/java/com/example/firstscreenleroy/MainActivity.kt
+++ app/src/main/java/com/example/firstscreenleroy/MainActivity.kt
@@ -1,10 +1,9 @@
 package com.example.firstscreenleroy
 
-import android.os.Build
 import android.os.Bundle
-import android.view.Window
-import android.view.WindowManager
 import androidx.appcompat.app.AppCompatActivity
+import com.squareup.picasso.Picasso
+import kotlinx.android.synthetic.main.activity_main.*
 
 
 class MainActivity : AppCompatActivity() {
@@ -12,11 +11,19 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-      //  if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
-         //   val w: Window = window
-
-            //w.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS)
-       // }
+        var image_url = arrayListOf<String>(
+            "https://res.cloudinary.com/lmru/image/upload/f_auto,q_90,h_240,c_pad,b_white,d_photoiscoming.png/LMCode/82325622.jpg",
+            "https://res.cloudinary.com/lmru/image/upload/f_auto,q_90,h_240,c_pad,b_white,d_photoiscoming.png/LMCode/82019008.jpg",
+            "https://res.cloudinary.com/lmru/image/upload/f_auto,q_90,h_240,c_pad,b_white,d_photoiscoming.png/LMCode/82187261.jpg",
+            "https://res.cloudinary.com/lmru/image/upload/f_auto,q_90,h_240,c_pad,b_white,d_photoiscoming.png/LMCode/81953829.jpg",
+            "https://res.cloudinary.com/lmru/image/upload/f_auto,q_90,h_240,c_pad,b_white,d_photoiscoming.png/LMCode/13654484.jpg",
+            "https://res.cloudinary.com/lmru/image/upload/f_auto,q_90,h_240,c_pad,b_white,d_photoiscoming.png/LMCode/82306061.jpg"
+        )
+        Picasso.get().load(image_url[1]).into(ivFirst)
+//        val displaymetrics = resources.displayMetrics
+//        var m = displaymetrics.widthPixels
+//        m += R.integer.width
+//    textView2.text = m.toString()
 
     }
 }
