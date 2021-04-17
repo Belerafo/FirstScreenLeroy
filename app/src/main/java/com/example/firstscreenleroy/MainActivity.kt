c3c2dc450b049b84acb4893fbbee18eaf766a1df
Lyshk <lyshka9@yandex.ru>
2021-04-17 11:20:13 +0300

    2

--- app/src/main/java/com/example/firstscreenleroy/MainActivity.kt
+++ app/src/main/java/com/example/firstscreenleroy/MainActivity.kt
@@ -1,9 +1,13 @@
 package com.example.firstscreenleroy
 
 import android.os.Bundle
+import android.view.View
+import android.widget.ImageView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.squareup.picasso.Picasso
 import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
 
 
 class MainActivity : AppCompatActivity() {
@@ -19,12 +23,22 @@ class MainActivity : AppCompatActivity() {
             "https://res.cloudinary.com/lmru/image/upload/f_auto,q_90,h_240,c_pad,b_white,d_photoiscoming.png/LMCode/13654484.jpg",
             "https://res.cloudinary.com/lmru/image/upload/f_auto,q_90,h_240,c_pad,b_white,d_photoiscoming.png/LMCode/82306061.jpg"
         )
-        Picasso.get().load(image_url[0]).into(iv2)
-        Picasso.get().load(image_url[1]).into(ivFirst)
+        val ivArray  = arrayListOf<ImageView>(iv1, iv2, iv3, iv4, iv5, iv6)
+        for (i in 0..5) {
+            Picasso.get().load(image_url[i]).into(ivArray[i])
+        }
+
+
 //        val displaymetrics = resources.displayMetrics
 //        var m = displaymetrics.widthPixels
 //        m += R.integer.width
 //    textView2.text = m.toString()
 
+    }
+
+
+    fun onClickButton(view: View) {
+      val toast =  Toast.makeText(applicationContext, "Кнопка нажата", Toast.LENGTH_SHORT)
+        toast.show()
     }
 }
